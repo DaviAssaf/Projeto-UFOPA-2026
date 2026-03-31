1b0a51a17b0f573c0c4ae9c66763638fe0b12f0a
DaviAssaf <daviassafmp1@gmail.com>
2026-03-31 19:12:16 -0300

    Ajustes do env

--- server/src/config/config.php
+++ server/src/config/config.php
@@ -1,11 +1,15 @@
 <?php
+require_once __DIR__ . '/env.php';
+loadEnv(__DIR__ . '/../../../.env');
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "ezopa2026";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$db = getenv('DB_NAME');
 
 $conn = new mysqli($host, $user, $password, $db);
+
+
 
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
