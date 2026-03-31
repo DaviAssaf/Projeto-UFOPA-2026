e00cc3c816fdc4ee58f8d97e143976b17ffcc495
DaviAssaf <daviassafmp1@gmail.com>
2026-03-31 19:48:24 -0300

    Mini-ajuste

--- server/src/config/env.php
+++ server/src/config/env.php
@@ -1,27 +1,18 @@
 <?php
 
-function loadEnv($path)
-{
-    if (!file_exists($path)) {
-        throw new Exception(".env file not found");
-    }
+$rows = file(__DIR__ . '../../../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($rows as $row) {
+    if (strpos(trim($row), '#') === 0) continue;
 
-    foreach ($lines as $line) {
-        if (str_starts_with(trim($line), '#')) {
-            continue;
-        }
+    list($name, $value) = explode('=', $row, 2);
 
-        [$key, $value] = explode('=', $line, 2);
+    $name = trim($name);
+    $value = trim($value);
 
-        $key = trim($key);
-        $value = trim($value);
-
-        $value = trim($value, '"\'');
-
-        putenv("$key=$value");
-        $_ENV[$key] = $value;
-        $_SERVER[$key] = $value;
+    if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+        putenv(sprintf('%s=%s', $name, $value));
+        $_ENV[$name] = $value;
+        $_SERVER[$name] = $value;
     }
 }
